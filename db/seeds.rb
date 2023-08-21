372fe604c96c7ab535be744b31e57eac6a57e118
nilay <nilayanand@gmail.com>
2023-08-21 11:56:14 +0530

    DB seeder reading data from CSV files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+CSV.foreach(Rails.root.join('db', 'seeder_data', 'merchants.csv'), headers: true, col_sep: ';') do |row|
+  Merchant.create!(row.to_hash)
+end
+
+# collect all Merchants reference and their id in Hash
+merchant_hash = Merchant.pluck(:reference, :id).to_h
+records = []
+CSV.foreach(Rails.root.join('db', 'seeder_data', 'orders.csv'), headers: true, col_sep: ';') do |row|
+  merchant_id = merchant_hash[row['merchant_reference']]
+  records << row.to_hash.except('merchant_reference').merge({ merchant_id: }) if merchant_id.present?
+end
+Order.insert_all records
